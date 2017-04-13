cf8b058432022737167bb8753f98b1569a69dbbb
Péter Ekler <peter.ekler@gmail.com>
2017-04-13 19:21:15 +0200

    AITForum with Firebase initial commit, register works

---
+++ AITForum/app/src/main/java/hu/ait/aitforum/LoginActivity.java
@@ -0,0 +1,125 @@
+package hu.ait.aitforum;
+
+import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
+import android.support.v4.text.TextUtilsCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.etEmail)
+    EditText etEmail;
+    @BindView(R.id.etPassword)
+    EditText etPassword;
+
+    private ProgressDialog progressDialog;
+    private FirebaseAuth firebaseAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btnRegister)
+    public void registerClick() {
+        if (!isFormValid()) {
+            return;
+        }
+
+        showProgressDialog();
+
+        firebaseAuth.createUserWithEmailAndPassword(
+                etEmail.getText().toString(), etPassword.getText().toString()
+        ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                hideProgressDialog();
+
+                if (task.isSuccessful()) {
+                    FirebaseUser firebaseUser = task.getResult().getUser();
+                    firebaseUser.updateProfile(
+                            new UserProfileChangeRequest.Builder().
+                                    setDisplayName(
+                                            userNameFromEmail(
+                                              firebaseUser.getEmail())).build()
+                    );
+
+                    Toast.makeText(LoginActivity.this, "REG OK",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(LoginActivity.this, "Failed: "+
+                            task.getException().getLocalizedMessage(),
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                hideProgressDialog();
+                Toast.makeText(LoginActivity.this,
+                        "error: "+e.getMessage(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+    public void showProgressDialog() {
+        if (progressDialog  == null) {
+            progressDialog = new ProgressDialog(this);
+            progressDialog.setMessage("Wait for it...");
+        }
+
+        progressDialog.show();
+    }
+
+    public void hideProgressDialog() {
+        if (progressDialog != null && progressDialog.isShowing()) {
+            progressDialog.hide();
+        }
+    }
+
+    private boolean isFormValid() {
+        if (TextUtils.isEmpty(etEmail.getText().toString())) {
+            etEmail.setError("This should not be empty");
+            return false;
+        }
+
+        if (TextUtils.isEmpty(etPassword.getText().toString())) {
+            etPassword.setError("This should not be empty");
+            return false;
+        }
+
+        return true;
+    }
+
+    private String userNameFromEmail(String email) {
+        if (email.contains("@")) {
+            return email.split("@")[0];
+        } else {
+            return email;
+        }
+    }
+}
